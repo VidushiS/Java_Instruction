18e1d86cc0f55240cf07e8eb8bc646d6aaf737e7
Vidushi <singh.vidushi@rocketmail.com>
2017-04-17 12:38:34 -0700

    Quadratic Formula
    This new version has tweaks regarding the discriminant, and can
    calculate negative discriminants now.

--- src/com/chsrobotics/javainstruction/QuadFactorCalc.java
+++ src/com/chsrobotics/javainstruction/QuadFactorCalc.java
@@ -1,26 +1,55 @@
 package com.chsrobotics.javainstruction;
 
 	// The statements below calculate the quadratic formula
-
+	
+	// Quadratic Equation: a*x*x + b*x + c = 0
 public class QuadFactorCalc {
 		
 	public static void main(String[] args){
 		//Insert the values of the equation below
 		double			a = 1;
-		double			b = 11;
-		double			c = 30;
+		double			b = 2;
+		double			c = 5;
 		
-		//This calculates everything for you!
-		double 			d = (-b + Math.sqrt((b*b)-(4*a*c)))/ (2*a);
-		double			e = (-b - Math.sqrt((b*b)-(4*a*c)))/ (2*a);
+		//This is the discriminant
+		double			discriminant = (b*b)-(4*a*c);
+		System.out.printf("The discriminant is equal to " + discriminant);
 		
-		//These are the discriminants
-		double			f = Math.sqrt((b*b)-(4*a*c));
-		double			g = Math.sqrt((b*b)-(4*a*c));
 		
-		System.out.printf("D is equal to " + d);
-		System.out.printf("   E is equal to " + e);
-		System.out.printf("   The discriminant is equal to " + f);
+		//Tells what to do when there is a regular discriminant
+		if(discriminant >= 0)
+			{
+				double 			d = -b + Math.sqrt(discriminant)/ (2*a);
+				double			e = -b - Math.sqrt(discriminant)/ (2*a);
+		
+				System.out.printf("   x is equal to " + d);
+				System.out.printf("   and x is equal to " + e);
+		
+			}
+		
+		//Tells what to do when there is a negative discriminant
+		if(discriminant < 0)
+			{
+				discriminant = discriminant/-1;
+				
+				
+				
+				double 			f = -b + Math.sqrt(discriminant);
+				double 			g = -b - Math.sqrt(discriminant);
+				double			h = 2*a;
+			
+				
+				
+				
+				System.out.printf("   x is equal to " + f + "i/" + h);
+				System.out.printf("   and x is equal to " + g + "i/" + h);
+			}
+		
+		
+		
+		
+		
+		
 		
 		
 		

c714158e366942706fd2023f496db2345ee1c477
Mark Burrell (ORF) <markrb060@gmail.com>
2017-04-18 13:30:39 -0700

    Fixed a typo and simplified a method

--- src/com/chsrobotics/javainstruction/Wednesday_Project.java
+++ src/com/chsrobotics/javainstruction/Wednesday_Project.java
@@ -1,17 +1,15 @@
 package com.chsrobotics.javainstruction;
 // This is an example class that solves a similar function to what the one you will be asked to solve is.
-// Your project is to solve f(x)=(x+4)^2)/x-4, 0<=X<=10
+// Your project is to solve f(x)=(x+4)^2)/x-3, 0<=X<=10
 public class Wednesday_Project {
-
-	static float input = 0;
 	public static float num(float given)
 	{
-		float output = (given + 4)*(given*3);
+		float output = (given + 4)*(given+4);
 		return output;
 	}
 	public static float den(float given)
 	{
-		float output=(given-1);
+		float output=(given-3);
 		return output;
 	}
 	public static float f(float x)
@@ -19,16 +17,15 @@ public class Wednesday_Project {
 		float output = num(x)/den(x);
 		return output;
 	}
-	public static void Method()
+	public static void Method(float input)
 	{
 		System.out.println("The Answer is: " + f(input));
 	}
 	public static void main(String args[])
 	{
-		for (int i=0; i <=15; i++)
+		for (int i=0; i <=10; i++)
 		{
-			Method();
-			input++;
-		}
+			Method(i);
+			}
 	}
 }

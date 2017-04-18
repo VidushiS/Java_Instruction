b11ae3c477cb74c12520901f6f8c17b5dae5b23d
Mark Burrell (ORF) <markrb060@gmail.com>
2017-04-18 13:09:52 -0700

    Update to Project for Thursday

--- src/com/chsrobotics/javainstruction/Wednesday_Project.java
+++ src/com/chsrobotics/javainstruction/Wednesday_Project.java
@@ -1,19 +1,34 @@
 package com.chsrobotics.javainstruction;
-
+// This is an example class that solves a similar function to what the one you will be asked to solve is.
+// Your project is to solve f(x)=(x+4)^2)/x-4, 0<=X<=10
 public class Wednesday_Project {
 
-	static float input = 5;
-public static float equation(float given)
-{
-	float output = (given * 5)/25;
-	return output;
-}
-public static void Method()
-{
-	System.out.println("The Answer is: " + equation(input));
-}
-public static void main(String args[])
-{
-	Method();
-}
+	static float input = 0;
+	public static float num(float given)
+	{
+		float output = (given + 4)*(given*3);
+		return output;
+	}
+	public static float den(float given)
+	{
+		float output=(given-1);
+		return output;
+	}
+	public static float f(float x)
+	{
+		float output = num(x)/den(x);
+		return output;
+	}
+	public static void Method()
+	{
+		System.out.println("The Answer is: " + f(input));
+	}
+	public static void main(String args[])
+	{
+		for (int i=0; i <=15; i++)
+		{
+			Method();
+			input++;
+		}
+	}
 }

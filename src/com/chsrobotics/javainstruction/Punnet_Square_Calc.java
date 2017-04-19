b8f0bcd8c0adc921c9bbb3502c5afc1bd473fb07
Vidushi <singh.vidushi@rocketmail.com>
2017-04-19 16:46:17 -0700

    Punnett Square Calculator
    This new version also tells you whether or not the alleles will display
    a dominant or reccessive phenotype

--- src/com/chsrobotics/javainstruction/Punnet_Square_Calc.java
+++ src/com/chsrobotics/javainstruction/Punnet_Square_Calc.java
@@ -2,18 +2,43 @@ package com.chsrobotics.javainstruction;
 
 public class Punnet_Square_Calc {
 	
-	static String				parent1Allele1 = "B"; //First allele in first parent
-	static String				parent1Allele2 = "b"; //Second allele in second parent
+	static String				parent1Allele1 = "W"; //First allele in first parent
+	static String				parent1Allele2 = "w"; //Second allele in second parent
 	
-	static String				parent2Allele1 = "b"; //First allele in second parent
-	static String				parent2Allele2 = "b"; // Second allele in second parent
+	static String				parent2Allele1 = "w"; //First allele in second parent
+	static String				parent2Allele2 = "w"; // Second allele in second parent
+	
+	public static boolean				p1A1dominant = true; // Is the 1st allele of parent 1 dominant?
+	public static boolean				p1A2dominant = false; // Is the 2nd allele of parent 1 dominant?
+	public static boolean				p2A1dominant = false; // Is the 1st allele of parent 2 dominant?
+	public static boolean				p2A2dominant = false; // Is the 2nd allele of parent 2 dominant?
 	
 	public static void main(String[] args)
 	{
-		System.out.println(parent1Allele1 + parent2Allele1);
-		System.out.println(parent1Allele1 + parent2Allele2);
-		System.out.println(parent1Allele2 + parent2Allele1);
-		System.out.println(parent1Allele2 + parent2Allele2);
+		if(p1A1dominant || p2A1dominant)
+		{
+			System.out.println(parent1Allele1 + parent2Allele1 + " displays the dominant phenotype");
+		}
+		else System.out.println(parent1Allele1 + parent2Allele1 + " displays the reccessive phenotype");
+		
+		if(p1A1dominant || p2A2dominant)
+		{
+			System.out.println(parent1Allele1 + parent2Allele2 + " displays the dominant phenotype");
+		}
+		else System.out.println(parent1Allele1 + parent2Allele2 + " displays the reccessive phenotype");
+		
+		if(p1A2dominant || p2A1dominant)
+		{
+			System.out.println(parent1Allele2 + parent2Allele1 + " displays the dominant phenotype");
+		}
+		else System.out.println(parent1Allele2 + parent2Allele1 + " displays the reccessive phenotype");
+		
+		if(p1A2dominant || p2A2dominant)
+		{
+			System.out.println(parent1Allele2 + parent2Allele2 + " displays the dominant phenotype");
+		}
+		else System.out.println(parent1Allele2 + parent2Allele2 + " displays the reccessive phenotype");
+		
 		System.out.println("These are the four combinations");
 		
 	}
